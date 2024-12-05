9d74563894985124578efd9c480d8c6d5eff3d11
kamils5555 <xkanzol@gmail.com>
2024-12-05 11:32:34 +0100

    +++
    +

--- E-CommerceStore/E-Commerce.Plugin.MySQL/obj/Debug/net8.0/E-Commerce.Plugin.MySQL.AssemblyInfo.cs
+++ E-CommerceStore/E-Commerce.Plugin.MySQL/obj/Debug/net8.0/E-Commerce.Plugin.MySQL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce.Plugin.MySQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bcb64454365a908f6dc5e07e038995650ea1e1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e049e8513eac1979e81f0a65b044e78ad9e8fab6")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce.Plugin.MySQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce.Plugin.MySQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
