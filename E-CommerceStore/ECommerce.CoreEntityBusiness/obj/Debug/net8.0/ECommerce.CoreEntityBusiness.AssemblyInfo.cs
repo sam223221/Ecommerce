e049e8513eac1979e81f0a65b044e78ad9e8fab6
kamils5555 <xkanzol@gmail.com>
2024-12-05 11:23:15 +0100

    first steps
    .

--- E-CommerceStore/ECommerce.CoreEntityBusiness/obj/Debug/net8.0/ECommerce.CoreEntityBusiness.AssemblyInfo.cs
+++ E-CommerceStore/ECommerce.CoreEntityBusiness/obj/Debug/net8.0/ECommerce.CoreEntityBusiness.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.CoreEntityBusiness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a204164728c0585b1cdbf8af95d4ca3a81e031b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bcb64454365a908f6dc5e07e038995650ea1e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.CoreEntityBusiness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.CoreEntityBusiness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
